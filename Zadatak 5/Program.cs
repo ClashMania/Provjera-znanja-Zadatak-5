2f6ace31c246bcd3a47e784d2c9556843c503774
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-01-25 08:50:46 +0100

    Add files via upload

---
+++ Zadatak 5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Definirajte klasu NebeskoTijelo sa sljedećim varijablama članicama:
+//• varijable tipa double
+//BrzinaOkoOsi 
+//BrzinaOkoSunca 
+//Definirajte klase Planet i Satelit izvedene iz klase NebeskoTijelo, te im definirajte sljedeće 
+//konstruktore:
+//Planet(double, double)
+//Satelit(double, double)
+//koji će dodijeliti vrijednosti proslijeđenih argumenata naslijeđenim varijablama članicama
+//Definirajte dva objekta Zemlja i Mjesec tipova Planet i Satelit.
+
+namespace Nasljedivanje1
+{
+    class NebeskoTijelo
+    {
+        double brzinaOkoOsi, brzinaOkoSunca;
+
+        public double BrzinaOkoOsi { get => brzinaOkoOsi; set => brzinaOkoOsi = value; }
+        public double BrzinaOkoSunca { get => brzinaOkoSunca; set => brzinaOkoSunca = value; }
+
+        public override string ToString()
+        {
+            return "Nebesko tijelo ima brzinu oko osi: " + BrzinaOkoOsi + " i ima brzinu oko sunca: " + BrzinaOkoSunca + ".";
+        }
+
+    }
+
+    class Planet : NebeskoTijelo
+    {
+        public Planet(double brzinaOkoOsi, double brzinaOkoSunca)
+        {
+            BrzinaOkoOsi = brzinaOkoOsi;
+            BrzinaOkoSunca = brzinaOkoSunca;
+        }
+
+        public override string ToString()
+        {
+            return "Planet ima brzinu oko osi: " + BrzinaOkoOsi + " i ima brzinu oko sunca: " + BrzinaOkoSunca + ".";
+        }
+
+    }
+
+    class Satelit : NebeskoTijelo
+    {
+        public Satelit(double brzinaOkoOsi, double brzinaOkoSunca)
+        {
+            BrzinaOkoOsi = brzinaOkoOsi;
+            BrzinaOkoSunca = brzinaOkoSunca;
+        }
+
+        public override string ToString()
+        {
+            return "Satelit ima brzinu oko osi: " + BrzinaOkoOsi + " i ima brzinu oko sunca: " + BrzinaOkoSunca + ".";
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Zemlja = new Planet(1656, 108000);
+            Satelit Mjesec = new Satelit(16.7, 108000);
+
+            Console.WriteLine(Zemlja.ToString());
+            Console.WriteLine(Mjesec.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
